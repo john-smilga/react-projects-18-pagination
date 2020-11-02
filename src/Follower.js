fa59ca9d7d85499f58f52c9f906e6ebab2a61427
john-smilga <johnsmilga@gmail.com>
2020-11-02 11:34:33 -0800

    dollar sign removed from Follower component

--- src/Follower.js
+++ src/Follower.js
@@ -4,7 +4,7 @@ const Follower = ({ avatar_url, html_url, login }) => {
   return (
     <article className='card'>
       <img src={avatar_url} alt={login} />
-      <h4>${login}</h4>
+      <h4>{login}</h4>
       <a href={html_url} className='btn'>
         view profile
       </a>
